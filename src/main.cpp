0e8bc4abb3fa8e73400c47c857398991afcd645a
Fayadh Hasri <hasri.fayadh@gmail.com>
2025-04-11 01:00:40 +0700

    style: adjust console output

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "header/InputHandler.hpp"
 #include "header/Image.hpp"
 #include "header/QuadTree.hpp"
+
+namespace fs = std::filesystem;
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -13,6 +15,7 @@ int main(int argc, char** argv) {
     try {
         int choice;
         while (true) {
+            cout << "========== INPUT ==========" << endl;
             cout << "Pilih Cara untuk Input: " << endl;
             cout << "1. Input dalam Satu Baris Sekaligus dengan format (<input path> <output path> <Metode> <threshold> <minBlockSize>)" << endl;
             cout << "2. Input Satu per Satu" << endl;
@@ -43,6 +46,8 @@ int main(int argc, char** argv) {
             return 1;
         }
 
+        uintmax_t sizeBefore = fs::file_size(io.getInputPath());
+
         cout << "Gambar dimuat: " << img.getWidth() << "x" << img.getHeight() << endl;
 
         auto start = chrono::high_resolution_clock::now();
@@ -62,7 +67,15 @@ int main(int argc, char** argv) {
             return 1;
         }
 
+        uintmax_t sizeAfter = fs::file_size(io.getOutputPath());
+
+        double compressionPercentage = (1.0 - static_cast<double>(sizeAfter) / sizeBefore) * 100.0;
+
+        cout << endl <<  "========== OUTPUT ==========" << endl;
         cout << "Gambar disimpan: " << io.getOutputPath() << endl;
+        cout << "Ukuran gambar sebelum kompresi: " << sizeBefore << " bytes" << endl;
+        cout << "Ukuran gambar setelah kompresi: " << sizeAfter << " bytes" << endl;
+        cout << "Persentase kompresi: " << compressionPercentage << "%" << endl;
         cout << "Waktu kompresi: " << duration << " ms" << endl;
         cout << "Kedalaman QuadTree: " << QT.getDepth() << endl;
         cout << "Jumlah node: " << QT.countNodes() << endl;
